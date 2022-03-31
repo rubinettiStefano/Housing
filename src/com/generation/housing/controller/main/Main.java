b60b9d70c41b938e48fe46c6d0b1091844d96dab
Rubinetti Stefano <stefano.rubinetti@gmail.com>
2022-03-31 15:25:09 +0200

    Commit 1

---
+++ src/com/generation/housing/controller/main/Main.java
@@ -0,0 +1,15 @@
+package com.generation.housing.controller.main;
+
+import com.generation.housing.model.entities.Room;
+
+public class Main 
+{
+    public static void main(String[] args)
+    {
+        Room r1 = new Room(5,6);
+        int sqm = 800; 
+
+        System.out.println("Total price:" + (r1.getArea()*sqm));
+
+    }    
+}
